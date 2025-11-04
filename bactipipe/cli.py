6233644e9d7abf757b0029257330e78e023aecde
Maurice Byukusenge <maurbyuka@gmail.com>
2025-11-04 10:11:06 -0500

    changed subcommand names

--- bactipipe/cli.py
+++ bactipipe/cli.py
@@ -19,9 +19,11 @@ def main():
       Update-packages  :  Update all Python/Conda packages.
       Update-databases :  Re-download the latest databases.
     \nPipeline commands:
-      QC-illumina      :  Run Illumina QC pipeline.
-      QC-nanopore      :  Run Nanopore QC pipeline.
-      Relate-genomes   :  Type assembled genomes and compute relatedness (ANI/SKA).
+      qc-illumina      :  Run Illumina QC pipeline.
+      qc-nanopore      :  Run Nanopore QC pipeline.
+      relate           :  Type assembled genomes and compute relatedness.
+      detect           :  Detect virulence and antimicrobial resistance genes in assemblies.
+
     \nUse 'bactipipe <command> -h' to see help for the selected pipeline. [Only for pipeline commands.]\n'''
 
     if len(sys.argv) < 2 or sys.argv[1] in ["-h", "--help"]:
@@ -38,6 +40,7 @@ def main():
         "qc-illumina": os.path.join(script_dir, "run_illumina_qc.py"),
         "qc-nanopore": os.path.join(script_dir, "run_nanopore_qc.py"),
         "relate": os.path.join(script_dir, "type_genomes.py"),
+        "detect": os.path.join(script_dir, "run_traits.py"),
     }
 
     if command not in script_map and command not in tool_manage_commands:
