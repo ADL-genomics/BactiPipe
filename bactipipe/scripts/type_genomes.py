a9e1d8e20ac82a602ec65af3801b2de74de7edde
Maurice Byukusenge <maurbyuka@gmail.com>
2026-01-23 10:12:45 -0500

    Some edits

--- bactipipe/scripts/type_genomes.py
+++ bactipipe/scripts/type_genomes.py
@@ -833,11 +833,19 @@ def _build_argparser() -> argparse.ArgumentParser:
                    default=os.environ.get("BACTIPIPE_ENV_CGE") or "genepid",
                    help=argparse.SUPPRESS)
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
     opt.add_argument("--cge-env",
                    default=os.environ.get("BACTIPIPE_ENV_CGE") or "genepid",
                    help=argparse.SUPPRESS)
 >>>>>>> 1ea388f (removed false AIDA (AIDA-like) records)
+=======
+    opt.add_argument("--cge-env",
+                   default=os.environ.get("BACTIPIPE_ENV_CGE") or "genepid",
+                   help=argparse.SUPPRESS)
+=======
+>>>>>>> 00d37d3 (added cgs_envs to opt.args)
+>>>>>>> 73cb978 (rebased)
 
     return p
 
